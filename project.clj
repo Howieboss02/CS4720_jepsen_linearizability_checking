6887a285cba8f8c8b01e4d385c738d9461ddf14d
Rafal Owczarski <R.R.Owczarski@student.tudelft.nl>
2025-06-08 16:50:33 +0200

    another approach

--- project.clj
+++ project.clj
@@ -1,14 +1,26 @@
-(defproject jepsen-redis "0.1.0-SNAPSHOT"
-  :description "Jepsen test for Redis with Sentinel failover"
-  :url "https://github.com/jepsen-io/jepsen"
+(defproject jepsen.redis-sentinel "0.1.0-SNAPSHOT"
+  :description "Jepsen tests for Redis Sentinel linearizability"
+  :url "https://github.com/your-username/jepsen.redis-sentinel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]          ; Updated from 1.5.1
-                 [jepsen "0.3.9"]                        ; Updated from 0.0.2-SNAPSHOT
-                 [com.taoensso/carmine "2.19.1"]  ; This version has better compatibility with older Sentinel setups
-                 [clj-time "0.15.2"]                     ; Updated from 0.5.1
-                 [org.clojure/tools.logging "1.2.4"]     ; Updated from 0.2.6
-                 [org.clojure/data.json "2.4.0"]]        ; Updated from 0.2.2
-  :main jepsen.redis
-  :aliases {"local" ["run" "-m" "jepsen.redis-local"]}
-  :profiles {:uberjar {:aot :all}})
+
+  ;; Core dependencies
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [jepsen "0.3.5"]
+                 [com.taoensso/carmine "3.2.0"]
+                 [cheshire "5.11.0"]
+                 [clj-time "0.15.2"]]
+
+  ;; Entry point
+  :main ^:skip-aot jepsen.redis-sentinel
+  :aot [jepsen.redis-sentinel]
+
+  ;; Source path setup
+  :source-paths ["src"]
+
+  ;; JVM tuning
+  :jvm-opts ["-Djava.awt.headless=true"
+             "-server"
+             "-Xmx4g"
+             "-XX:+UseG1GC"
+             "-XX:MaxGCPauseMillis=50"])
